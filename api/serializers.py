645928fc23681ba27edefd147a5810f13393b406
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-17 14:02:28 +0530

    fix seroalozer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from .models import Student, Subject, Mark
+from rest_framework import serializers
 
-class StudentSerializer:
+class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ("id", "fname", "lname", "roll_no", "class_div")
