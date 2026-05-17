618f74c2925628ab39a336666b86699cb43f4d8a
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-17 13:55:12 +0530

    xif

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from models import Student
+from .models import Student
 from .serializers import StudentSerializer
 
 # Create your views here.
