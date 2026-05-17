24ba3d102c5d1a505f3593c634cdb8ef61f5bfd9
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-17 14:03:52 +0530

    change fields

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    fname = models.CharField(max_length=100, blank=False)
-    lname = models.CharField(max_length=100, blank=False)
+    surname = models.CharField(max_length=100, blank=False)
+    first_name = models.CharField(max_length=100, blank=False)
     roll_no = models.IntegerField(blank=False)
     class_div = models.CharField(max_length = 4, blank=False)
 
--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ("id", "fname", "lname", "roll_no", "class_div")
+        fields = ("id", "first_name", "surname", "roll_no", "class_div")
