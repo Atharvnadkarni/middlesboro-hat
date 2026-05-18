b5dcca7ce7e5740d180a021abb6273688b0eca85
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-18 14:07:51 +0530

    config webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-    entry: "./src/main.js",
-    output: {
-        path: __dirname
-    }
-}
+  entry: "./src/main.js",
+  output: {
+    path: __dirname + "/static",
+    filename: "index.js",
+    publicPath: "/static",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node-modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};
