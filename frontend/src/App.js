55f5779ff013915b582f3efe697c3b5291609846
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-19 22:05:06 +0530

    make subjectList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import { Container } from "@mui/material";
 import Header from "./components/Header";
+import SubjectList from "./components/SubjectList";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Container sx={{marginTop: 3}}>
+        <SubjectList />
+      </Container>
     </div>
   );
 };
---
+++ frontend/src/components/SubjectList.js
@@ -0,0 +1,21 @@
+import { DataGrid } from "@mui/x-data-grid";
+const SubjectList = () => {
+  const columns = [
+    { field: "roll_no", headerName: "Roll No", width: 20 },
+    { field: "name", headerName: "Name", width: 200, editable: true },
+    { field: "math", headerName: "Math", width: 90, editable: true },
+    { field: "english", headerName: "English", width: 90, editable: true },
+    { field: "hindi", headerName: "Hindi", width: 90, editable: true },
+    { field: "science", headerName: "Science", width: 90, editable: true },
+    { field: "ss", headerName: "SS", width: 90, editable: true },
+    { field: "hs", headerName: "Home Sci", width: 90, editable: true },
+    { field: "it", headerName: "Info Tech", width: 90, editable: true },
+    { field: "hc", headerName: "Healthcare", width: 90, editable: true },
+    { field: "pt", headerName: "Painting", width: 90, editable: true },
+    { field: "ai", headerName: "AI", width: 90, editable: true },
+  ];
+  // const columns = {}
+  const rows = [{ id: 1, roll_no: 1, name: "Mehr Kahlon", math: 97.5, english: 91.875, hindi: 100, science:100, ss: 91.8}];
+  return <DataGrid columns={columns} rows={rows}>{console.log(columns, rows)}</DataGrid>;
+};
+export default SubjectList;
