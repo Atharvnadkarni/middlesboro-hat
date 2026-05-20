0bade0aa347bd3b73d67fb8b1a7d31f96b8c9a5d
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-20 19:43:22 +0530

    fix migrations and stuff

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ class Student(models.Model):
     surname = models.CharField(max_length=100, blank=False)
     first_name = models.CharField(max_length=100, blank=False)
     roll_no = models.IntegerField(blank=False)
-    class_div = models.ForeignKey(to=Class, on_delete=models.CASCADE)
+    class_div = models.ForeignKey(to="Class", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.first_name} {self.surname}"
@@ -33,11 +33,11 @@ class Class(models.Model):
 
 
 class Role(models.Model):
-    role = models.CharField()
+    role = models.CharField(max_length=50)
 
 
 class Teacher(models.Model):
     subject = models.ManyToManyField(to=Subject)
-    sub_classes = models.ManyToManyField(to=Class)
+    sub_classes = models.ManyToManyField(to="Class", related_name="subject_classes")
     role = models.ForeignKey(to=Role, on_delete=models.SET_NULL, null=True)
-    class_tr = models.ForeignKey(to=Class, on_delete=models.SET_NULL, null=True, blank=True)
+    class_tr = models.ForeignKey(to="Class", on_delete=models.SET_NULL, null=True, blank=True, related_name="teacher_class")
