10397753311724c86d5a9e4451db7022bf12713d
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-20 21:57:20 +0530

    add model and seri

--- api/models.py
+++ api/models.py
@@ -30,14 +30,21 @@ class Mark(models.Model):
 class Class(models.Model):
     grade = models.IntegerField()
     division = models.CharField(max_length=1)
+    
+    def __str__(self):
+        return f"{self.grade}{self.division}"
 
 
 class Role(models.Model):
     role = models.CharField(max_length=50)
+    def __str__(self):
+        return f"{self.role}"
 
 
 class Teacher(models.Model):
-    subject = models.ManyToManyField(to=Subject)
-    sub_classes = models.ManyToManyField(to="Class", related_name="subject_classes")
+    first_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    subject = models.ManyToManyField(to=Subject, null=True, blank=True)
+    sub_classes = models.ManyToManyField(to="Class", related_name="subject_classes", null=True, blank=True)
     role = models.ForeignKey(to=Role, on_delete=models.SET_NULL, null=True)
     class_tr = models.ForeignKey(to="Class", on_delete=models.SET_NULL, null=True, blank=True, related_name="teacher_class")
