5062217e76a8bb1eff4e62fdb1657df1c33cdc2d
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-20 21:56:10 +0530

    add test view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import CreateStudentView, StudentMarkListView, MarkUpdateView
+from .views import CreateStudentView, StudentMarkListView, MarkUpdateView, CreateClassView, CreateTeacherView, CreateSubjectView, RoleViewCreate
 
 urlpatterns = [
     path('home/', CreateStudentView.as_view()),
     path('marks/', StudentMarkListView.as_view()),
     path('mark/', MarkUpdateView.as_view()),
+    path('class/', CreateClassView.as_view()),
+    path('teacher/', CreateTeacherView.as_view()),
+    path('subject/', CreateSubjectView.as_view()),
+    path('role/', RoleViewCreate.as_view()),
 ]
