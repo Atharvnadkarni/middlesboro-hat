916df6cc272f16ab80a4cc368434fe31fd30b87b
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-20 22:56:59 +0530

    fix

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,6 +2,7 @@ import { Container } from "@mui/material";
 import Tabs from "../components/Tabs";
 import Dropdown from "../components/Dropdown";
 import SubjectList from "../components/SubjectList";
+import { useEffect, useState } from "react";
 
 const Home = () => {
     const [classe, setClass] = useState();
