531834f948ca1ad5871505fec49f87e3bcecaaba
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-20 23:14:14 +0530

    xif2

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -5,15 +5,15 @@ import SubjectList from "../components/SubjectList";
 import { useEffect, useState } from "react";
 
 const Home = () => {
-    const [classe, setClass] = useState();
+  const [classe, setClass] = useState();
   useEffect(() => {
     localStorage.setItem("class", classe);
   }, [classe]);
   return (
-
+    <>
       <Dropdown setClass={setClass} />
       <SubjectList class={classe} />
-
+    </>
   );
 };
 export default Home;
