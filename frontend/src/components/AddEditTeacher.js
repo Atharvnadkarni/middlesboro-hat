d6fd344f0b1da0cda5ca7fbc48178a286f836195
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-24 12:17:00 +0530

    add modal work

--- frontend/src/components/AddEditTeacher.js
+++ frontend/src/components/AddEditTeacher.js
@@ -1,33 +1,90 @@
 import {
+  Button,
   Dialog,
   DialogContent,
   DialogTitle,
+  FormControl,
+  FormHelperText,
   Grid,
   IconButton,
+  InputLabel,
   MenuItem,
   Select,
   TextField,
   Typography,
 } from "@mui/material";
 import { Add } from "@mui/icons-material";
-import { useState } from "react";
+import { useId, useState } from "react";
+import axios from "axios";
 
 const AddEditTeacher = () => {
+  const subjectList = [
+    "Math",
+    "English",
+    "Hindi",
+    "Sci",
+    "French",
+    "SS",
+    "HS",
+    "Painting",
+    "HC",
+    "AI",
+    "IT",
+    "PE",
+    "Yoga",
+    "NSS",
+    "MA",
+    "Comp",
+    "WE",
+    "ATL",
+    "Art",
+    "Music",
+    "SD",
+  ];
+
+  const classId = useId();
+  const roleId = useId();
+
+  const [firstName, setFirstName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [classTr, setClassTr] = useState("");
+  const [role, setRole] = useState("");
   const [subjects, setSubjects] = useState([]);
+
   const [formData, setFormData] = useState({
-    selectValue: "English",
-    classValue: "10A",
+    selectValue: subjectList[0],
+    classValue: "10ABC",
   });
+  const handleSubmit = async () => {
+    const res = await axios.post({
+      firstName,
+      surname,
+      classTr,
+      subjects,
+      role,
+    });
+    location.reload();
+  };
   return (
     <Dialog open={true}>
       <DialogTitle>Add Teacher</DialogTitle>
       <DialogContent>
         <Grid container spacing={2} sx={{ padding: 2 }}>
           <Grid size={{ xs: 6 }}>
-            <TextField fullWidth label="First Name" />
+            <TextField
+              fullWidth
+              label="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </Grid>
           <Grid size={{ xs: 6 }}>
-            <TextField fullWidth label="Surname" />
+            <TextField
+              fullWidth
+              label="Surname"
+              value={surname}
+              onChange={(e) => setSurname(e.target.value)}
+            />
           </Grid>
         </Grid>
         <Grid container spacing={2} sx={{ padding: 2, alignItems: "center" }}>
@@ -39,47 +96,27 @@ const AddEditTeacher = () => {
                 setFormData((ofd) => ({ ...ofd, selectValue: e.target.value }));
               }}
             >
-              <MenuItem value={"English"}>English</MenuItem>
-              <MenuItem value={"Math"}>Math</MenuItem>
-              <MenuItem value={"Science"}>Science</MenuItem>
-              <MenuItem value={"Social Science"}>Social Science</MenuItem>
-              <MenuItem value={"Hindi"}>Hindi</MenuItem>
-              <MenuItem value={"French"}>French</MenuItem>
-              <MenuItem value={"Home Science"}>Home Science</MenuItem>
-              <MenuItem value={"Healthcare"}>Healthcare</MenuItem>
-              <MenuItem value={"Painting"}>Painting</MenuItem>
-              <MenuItem value={"Information Technology"}>IT</MenuItem>
-              <MenuItem value={"Artificial Intelligence"}>
-                Artificial Intelligence
-              </MenuItem>
-              <MenuItem value={"Computer"}>Computer Education</MenuItem>
-              <MenuItem value={"PE"}>Physical Education</MenuItem>
-              <MenuItem value={"Yoga"}>Yoga</MenuItem>
-              <MenuItem value={"MA"}>MA</MenuItem>
-              <MenuItem value={"NSS"}>NSS</MenuItem>
-              <MenuItem value={"ATL"}>ATL</MenuItem>
-              <MenuItem value={"WE"}>WE</MenuItem>
-              <MenuItem value={"Art"}>Art</MenuItem>
-              <MenuItem value={"Music"}>Music</MenuItem>
-              <MenuItem value={"SD"}>SD</MenuItem>
+              {subjectList.map((sub) => (
+                <MenuItem value={sub}>{sub}</MenuItem>
+              ))}
               {/* <MenuItem value={"Dance"}>Dance</MenuItem> */}
             </Select>
           </Grid>
           <Grid size={{ xs: 4 }}>
             <Select
               fullWidth
-              value={formData.classValue || "10A"}
+              value={formData.classValue || "10ABC"}
               onChange={(e) => {
                 setFormData((ofd) => ({ ...ofd, classValue: e.target.value }));
               }}
             >
-              <MenuItem value={"10A"}>10A</MenuItem>
-              <MenuItem value={"10B"}>10B</MenuItem>
-              <MenuItem value={"10C"}>10C</MenuItem>
+              <MenuItem value={"10ABC"}>10ABC</MenuItem>
               <MenuItem value={"10AB"}>10AB</MenuItem>
               <MenuItem value={"10AC"}>10AC</MenuItem>
               <MenuItem value={"10BC"}>10BC</MenuItem>
-              <MenuItem value={"10ABC"}>10ABC</MenuItem>
+              <MenuItem value={"10A"}>10A</MenuItem>
+              <MenuItem value={"10B"}>10B</MenuItem>
+              <MenuItem value={"10C"}>10C</MenuItem>
               {/* <MenuItem value={"Dance"}>Dance</MenuItem> */}
             </Select>
           </Grid>
@@ -99,6 +136,10 @@ const AddEditTeacher = () => {
                       classes: formData.classValue,
                     },
                   ]);
+                setFormData({
+                  selectValue: subjectList[0],
+                  classValue: "10ABC",
+                });
               }}
             >
               <Add />
@@ -119,6 +160,52 @@ const AddEditTeacher = () => {
             </Grid>
           </Grid>
         ))}
+        <Grid container spacing={2} sx={{ padding: 2, alignItems: "center" }}>
+          <Grid size={{ xs: 6 }}>
+            <FormControl fullWidth>
+              <InputLabel>Role</InputLabel>
+              <Select
+                fullWidth
+                // labelId={`${roleId}-label`}
+                // id={roleId}
+                label="Role"
+                value={role}
+                onChange={(e) => {
+                  setRole(e.target.value);
+                  if (e.target.value == "Administrator") setClassTr("No");
+                }}
+              >
+                <MenuItem value="Administrator">Administrator</MenuItem>
+                <MenuItem value="Teacher">Teacher</MenuItem>
+              </Select>
+              <FormHelperText>Teacher Role</FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid size={{ xs: 6 }}>
+            <FormControl fullWidth disabled={role == "Administrator"}>
+              <InputLabel>Class</InputLabel>
+              <Select
+                fullWidth
+                label="Class"
+                value={classTr}
+                onChange={(e) => {
+                  setClassTr(e.target.value);
+                }}
+              >
+                <MenuItem value="No">No</MenuItem>
+                <MenuItem value="10A">10A</MenuItem>
+                <MenuItem value="10B">10B</MenuItem>
+                <MenuItem value="10C">10C</MenuItem>
+              </Select>
+              <FormHelperText>Class teacher?</FormHelperText>
+            </FormControl>
+          </Grid>
+        </Grid>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button variant="contained" onClick={handleSubmit}>
+            Add Teacher
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
