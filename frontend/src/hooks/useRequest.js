1beb4d394f7efd4b9cf0e4eecff687d7808aa153
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-24 12:58:00 +0530

    add hook

---
+++ frontend/src/hooks/useRequest.js
@@ -0,0 +1,33 @@
+import { useCallback, useState } from "react";
+import axios from "axios";
+
+export const useRequest = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const request = useCallback(async (
+    method,
+    url,
+    data = {},
+    headers = {},
+    config = { BASE_URL: "http://localhost:8000" },
+  ) => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const res = await axios({
+        method,
+        url: `${config.BASE_URL}/${url}/`,
+        data,
+        headers,
+      });
+      return res.data;
+    } catch (err) {
+      setError(err.message);
+      throw err;
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  return { request, isLoading, error };
+};
