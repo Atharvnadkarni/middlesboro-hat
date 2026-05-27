8b1d631eef9094d2cb2c0441150d066d6871d00e
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-27 18:45:22 +0530

    Revert "Revert "otpimise""
    This reverts commit edd4bc0f16fd1a85fd91316475282746a79d1b0a.

--- frontend/src/pages/teachers.js
+++ frontend/src/pages/teachers.js
@@ -4,10 +4,13 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { PersonAdd } from "@mui/icons-material";
 import AddEditTeacher from "../components/AddEditTeacher";
+import DeleteModal from "../components/DeleteModal";
 
 const Teachers = () => {
   const [teachers, setTeachers] = useState([]);
   const [editingTeacher, setEditingTeacher] = useState({});
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   useEffect(() => {
     (async () => {
       const res = await axios.get("/api/teacher");
@@ -33,7 +36,13 @@ const Teachers = () => {
       </div>
       <Grid container spacing={2}>
         {teachers.map((teacher) => (
-          <TeacherCard key={teacher.id} teacher={teacher} onSetEdit={setEditingTeacher} />
+          <TeacherCard
+            key={teacher.id}
+            teacher={teacher}
+            setEdit={setEditingTeacher}
+            setEditModalOpen={setEditModalOpen}
+            setDeleteModalOpen={setDeleteModalOpen}
+          />
         ))}
       </Grid>
       <AddEditTeacher
