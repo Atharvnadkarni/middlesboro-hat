3a5158037448a909810938f3acebbb7a4df8fc80
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-28 17:09:26 +0530

    add skelton

--- frontend/src/pages/teachers.js
+++ frontend/src/pages/teachers.js
@@ -1,17 +1,19 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Skeleton } from "@mui/material";
 import TeacherCard from "../components/TeacherCard";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { PersonAdd } from "@mui/icons-material";
 import AddEditTeacher from "../components/AddEditTeacher";
 import Tabs from "../components/Tabs";
+import { useRequest } from "../hooks/useRequest";
 
 const Teachers = () => {
   const [teachers, setTeachers] = useState([]);
   const [editingTeacher, setEditingTeacher] = useState({});
+  const { request, isLoading, error } = useRequest();
   useEffect(() => {
     (async () => {
-      const res = await axios.get("/api/teacher");
+      const res = await request("get", "/api/teacher");
       const data = await res.data;
       setTeachers(data);
     })();
@@ -32,12 +34,27 @@ const Teachers = () => {
           Add Teacher
         </Button>
       </div>
+
       <Grid container spacing={2}>
         {teachers.map((teacher) => (
-          <TeacherCard key={teacher.id} teacher={teacher} onSetEdit={setEditingTeacher} />
+          <TeacherCard
+            key={teacher.id}
+            teacher={teacher}
+            onSetEdit={setEditingTeacher}
+          />
         ))}
+        {isLoading &&
+          [1, 2, 3, 4, 5, 6, 7, 8,9,10].map((a) => (
+            <Grid item size={{ xs: 3 }}>
+              <Skeleton variant="rectangular" height={130} animation="wave" />
+            </Grid>
+          ))}
       </Grid>
-      <AddEditTeacher open={modalOpen} setOpen={setModalOpen} mode={{mode: "add"}} />
+      <AddEditTeacher
+        open={modalOpen}
+        setOpen={setModalOpen}
+        mode={{ mode: "add" }}
+      />
     </>
   );
 };
