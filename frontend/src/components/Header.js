75d648b12fd18604404320acda5a4edf4bf48337
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-05-28 15:29:00 +0530

    add logout request

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
 import { CalendarMonth } from "@mui/icons-material";
+import HelloTr from "./HelloTr";
 const Header = () => {
   return (
     <AppBar position="static">
       <Container>
-        <Toolbar disableGutters>
-          <CalendarMonth />
-          <Typography variant="h6">Marksheet</Typography>
+        <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
+          <Box>
+            <CalendarMonth />
+            <Typography variant="h6">Marksheet</Typography>
+          </Box>
+          <Box>
+            <HelloTr />
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
---
+++ frontend/src/components/HelloTr.js
@@ -0,0 +1,92 @@
+import { ArrowDownward, ArrowDropDown, Logout } from "@mui/icons-material";
+import {useRequest} from "../hooks/useRequest"
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+
+const HelloTr = () => {
+  const [profile, setProfile] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleOpen = (e) => setAnchorEl(e.currentTarget);
+  const handleClose = (e) => setAnchorEl(null);
+  const {request, isLoading, error} = useRequest()
+  const logout = async () => {
+    await request("get", "/api/logout")
+  }
+  useEffect(() => {
+    if (localStorage.getItem("profile")) {
+      const profile = JSON.parse(localStorage.getItem("profile"));
+      setProfile(profile);
+    }
+  }, []);
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+  }
+  function stringAvatar(name) {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+    };
+  }
+  return (
+    <>
+      <IconButton
+        variant="standard"
+        onClick={!!anchorEl ? handleClose : handleOpen}
+      >
+        {profile?.first_name && (
+          <Avatar
+            {...stringAvatar(`${profile.first_name} ${profile.surname}`)}
+          />
+        )}
+      </IconButton>
+      <Menu open={!!anchorEl} anchorEl={anchorEl} onClose={handleClose}>
+        <MenuItem>
+          <Stack>
+            <Typography variant="subtitle1">
+              {profile?.first_name} {profile.surname}
+            </Typography>
+            <Typography variant="body2">{profile?.role}</Typography>
+          </Stack>
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={logout}>
+          <ListItemIcon>
+            <Logout />
+          </ListItemIcon>
+          <ListItemText>Logout</ListItemText>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+export default HelloTr;
