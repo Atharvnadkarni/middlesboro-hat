784a8eed0691e0a50583bddf8ece3ba0e40410ae
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-06-02 17:29:38 +0530

    bodipy tabs

--- frontend/src/components/Tabs.js
+++ frontend/src/components/Tabs.js
@@ -1,20 +1,26 @@
-import { Tab, Tabs as TabContainer } from "@mui/material"
+import { Tab, Tabs as TabContainer } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
-const Tabs = ({value: valueData}) => {
-    const [value, setValue] = useState(0);
-    useEffect(() => {
-      if (valueData) setValue(value)
-    }, [])
-    const handleChange = (e, newValue) => {
-        setValue(newValue)
-    }
+const Tabs = ({ value: valueData }) => {
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log(997, valueData);
+    if (valueData) setValue(valueData);
+  }, [valueData]);
+  const handleChange = (e, newValue) => {
+    setValue(newValue);
+  };
+  console.log(valueData, value, 997);
   return (
-   <TabContainer value={value} onChange={handleChange} sx={{marginBottom:2}}>
-    <Tab label="marktable" component={Link} to="/" />
-    <Tab label="teachers" component={Link} to="/teachers" />
-   </TabContainer>
-  )
-}
-export default Tabs
+    <TabContainer
+      value={value}
+      onChange={handleChange}
+      sx={{ marginBottom: 2 }}
+    >
+      <Tab value="marktable" label="marktable" component={Link} to="/" />
+      <Tab value="teachers" label="teachers" component={Link} to="/teachers" />
+    </TabContainer>
+  );
+};
+export default Tabs;
