993d5f003e06b803ef0afdd4d1f17b73844f812c
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-06-05 16:44:48 +0530

    modifyuu

--- frontend/src/context/slices/studentSlice.js
+++ frontend/src/context/slices/studentSlice.js
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const studentSlice = createSlice({
   name: "students",
-  initialState: {},
+  initialState: [],
 
   reducers: {
     setStudentsValue: (state, action) => {
