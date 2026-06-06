d0c6fe597709b03d46b4f2fa9a18fd73b7da7692
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-06-06 13:04:47 +0530

    add quicktable

--- frontend/src/components/MarksheetTemplate.js
+++ frontend/src/components/MarksheetTemplate.js
@@ -1,35 +1,144 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
+const QuickTable = ({ subject, exam, class: classe, cols, row, children }) => {
+  return (
+    <table className="quickTable">
+      <thead>
+        <tr>
+          <th colSpan={4}>Vidya Vikas Academy</th>
+        </tr>
+        <tr>
+          <th colSpan={4}>
+            {subject} {exam} Marklist Of Class {classe}
+          </th>
+        </tr>
+      </thead>
+      {children}
+    </table>
+  );
+};
+
 const MarksheetTemplate = () => {
   const {
     class: classe,
     exam,
     marksheet: { rows, columns },
+    format: { format, subject: formatSubject, class: formatClass },
+    student: students,
   } = useSelector((state) => state);
-  const format = useSelector((state) => state.format);
+  console.log(exam, "ytqat")
 
-  useEffect(() => {
-    console.log(format);
-  }, [format]);
+  console.log(students, 9991);
+
   const [profile, setProfile] = useState({});
   useEffect(() => {
     const profileStr = localStorage.getItem("profile");
     setProfile(JSON.parse(profileStr));
   }, []);
-  return (
-    <table border="1">
-      <thead>
-        <tr>
-          <td colSpan={4}>Vidya Vikas Academy</td>
-        </tr>
-        <tr>
-          <td colSpan={4}>
-            {JSON.stringify(format)} {exam} Marksheet
-          </td>
-        </tr>
-      </thead>
-    </table>
-  );
+
+  const pt1IndividualSubject = () => {
+    return (
+      <QuickTable subject={formatSubject} exam={exam} class={formatClass}>
+        <thead>
+          <tr>
+            <th>Roll No</th>
+            <th>Surname</th>
+            <th>First Name</th>
+            <th>Marks/20</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students &&
+            students.map((student) => (
+              <tr>
+                <td>{student.roll_no}</td>
+                <td>{student.surname}</td>
+                <td>{student.first_name}</td>
+                <td>
+                  {
+                    student.marks.filter(
+                      (m) =>
+                        m.exam.abbreviation == exam &&
+                        m.subject.sub == formatSubject,
+                    )[0].score
+                  }
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </QuickTable>
+    );
+  };
+  const midTermsPBIndividualSubject = () => {
+    return (
+      <QuickTable subject={formatSubject} exam={exam} class={formatClass}>
+        <thead>
+          <tr>
+            <th>Roll No</th>
+            <th>Surname</th>
+            <th>First Name</th>
+            <th colSpan={3}>{formatSubject}</th>
+          </tr>
+          <tr>
+            <th colSpan={3}></th>
+            <th>MO/80</th>
+            <th>MO/100</th>
+            <th>GRADE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students &&
+            students.map((student) => (
+              <tr>
+                <td>{student.roll_no}</td>
+                <td>{student.surname}</td>
+                <td>{student.first_name}</td>
+                <td>
+                  {
+                    student.marks.filter(
+                      (m) =>
+                        m.exam.abbreviation == exam &&
+                        m.subject.sub == formatSubject,
+                    )[0].score
+                  }
+                </td>
+                <td>
+                  {(student.marks.filter(
+                    (m) =>
+                      m.exam.abbreviation == exam &&
+                      m.subject.sub == formatSubject,
+                  )[0].score *
+                    5) /
+                    4}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </QuickTable>
+    );
+  };
+  const formatTable = {
+    individual: {
+      PT1: pt1IndividualSubject,
+      PT2: pt1IndividualSubject,
+      PT3: pt1IndividualSubject,
+      MT: midTermsPBIndividualSubject,
+      PB1: midTermsPBIndividualSubject,
+      PB2: midTermsPBIndividualSubject,
+      PB3: midTermsPBIndividualSubject,
+    },
+    consolidated: {
+      PT1: pt1IndividualSubject,
+      PT2: pt1IndividualSubject,
+      PT3: pt1IndividualSubject,
+      MT: midTermsPBIndividualSubject,
+      PB1: midTermsPBIndividualSubject,
+      PB2: midTermsPBIndividualSubject,
+      PB3: midTermsPBIndividualSubject,
+    },
+  };
+
+  return formatTable[format][exam]();
 };
 export default MarksheetTemplate;
