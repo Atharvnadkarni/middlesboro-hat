fb7c0e3a4cb736f0f28fcd2839c656912f67bacb
Atharv Nadkarni <atharvmnadkarni@gmail.com>
2026-06-11 17:08:15 +0530

    dool

--- marksheet/settings.py
+++ marksheet/settings.py
@@ -149,4 +149,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOL = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
